1082628a4fb79d344edaede2cb252a61ca10b962
Silvano AP <silvanoeng@hotmail.com>
2021-10-03 23:02:23 -0300

    Aula 64

--- src/poo/intellij/Main.java
+++ src/poo/intellij/Main.java
@@ -6,16 +6,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String s1, s2, s3;
+//        s1 = sc.nextLine();
+//        s2 = sc.nextLine();
+//        s3 = sc.nextLine();
+//        System.out.println("DADOS DIGITADOS:");
+//        System.out.println(s1);
+//        System.out.println(s2);
+//        System.out.println(s3);
+//        sc.close();
         Scanner sc = new Scanner(System.in);
-        String s1, s2, s3;
-        s1 = sc.nextLine();
-        s2 = sc.nextLine();
-        s3 = sc.nextLine();
-        System.out.println("DADOS DIGITADOS:");
-        System.out.println(s1);
-        System.out.println(s2);
-        System.out.println(s3);
-        sc.close();
+
+        int n, acum;
+        n = sc.nextInt();
+        if (n > 1) {
+            acum = n;
+            for (int i = n; i > 1; i--) {
+                acum = ((i - 1) * acum);
+
+            }
+            System.out.print(acum);
+        } else {
+            System.out.print(n);
+        }
     }
 }
 
