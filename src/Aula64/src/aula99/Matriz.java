976b6967294c7088409daee8da453bf33c523b40
Silvano AP <silvanoeng@hotmail.com>
2021-10-13 17:04:32 -0300

    Atualizando exercicio

--- src/Aula64/src/aula99/Matriz.java
+++ src/Aula64/src/aula99/Matriz.java
@@ -9,22 +9,22 @@ public class Matriz {
         System.out.println("Qual o tamanho da matriz: ");
         int n = sc.nextInt();
         int [][] matriz = new int[n][n];
-        for (int li=0; li<n; li++){
-            for (int co=0; co<n; co++){
+        for (int li=0; li<matriz.length; li++){
+            for (int co=0; co<matriz[li].length; co++){
                 System.out.println("Digite o valor para a posição (linha: "+li+" - coluna: "+co );
                 matriz[li][co]= sc.nextInt();
             }
         }
         System.out.println("-----------------------------------------------------");
 
-        for (int i=0;i<n;i++){
+        for (int i=0;i<matriz.length;i++){
             System.out.print(matriz[i][i]+" ");
         }
 
         System.out.println("\n-----------------------------------------------------");
         int cont=0;
-        for (int li=0; li<n; li++){
-            for (int co=0; co<n; co++){
+        for (int li=0; li<matriz.length; li++){
+            for (int co=0; co<matriz[li].length; co++){
                 if(matriz[li][co]<0) {
                  cont++;
                 }
@@ -32,8 +32,8 @@ public class Matriz {
         }
         System.out.println(cont +" é a quantidade de números negativos na matriz.");
         System.out.println("-----------------------------------------------------");
-        for (int li=0; li<n; li++){
-            for (int co=0; co<n; co++){
+        for (int li=0; li<matriz.length; li++){
+            for (int co=0; co<matriz[li].length; co++){
                 System.out.print(matriz[li][co]+" ");
             }
             System.out.println("\n");
